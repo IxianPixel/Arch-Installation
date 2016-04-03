958bfc3db2608dd6679394a18dda39e585749710
IxianPixel <dylanaddison@gmail.com>
2016-04-04 00:52:48 +0100

    Minor corrections to font installer, added .vimrc

---
+++ dotfiles/.vimrc
@@ -0,0 +1,10 @@
+filetype plugin indent on
+syntax on
+set number
+
+" Plugin Begin
+call plug#begin()
+Plug 'pangloss/vim-javascript'
+Plug 'tpope/vim-markdown'
+Plug 'scrooloose/syntastic'
+call plug#end()
--- fonts/install-fonts.sh
+++ fonts/install-fonts.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-sudo pacman -Syu ttf-bitstram-vera ttf-inconsolata ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-linux-libertine ttf-liberation
+sudo pacman -Syu ttf-bitstream-vera ttf-inconsolata ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-linux-libertine ttf-liberation
 
-yaourt -S ttf-ms-fonts ttf-vista-fonts ttf-monaco﻿ ttf-qurancomplex-fonts
+yaourt -S ttf-ms-fonts
+yaourt -S ttf-vista-fonts
+yaourt -S ttf-monaco
+yaourt -S ttf-qurancomplex-fonts
+yaourt -S powerline-fonts-git
 
 sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
 
